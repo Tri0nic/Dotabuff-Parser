ce395709d88772c4272ab4d06d91ce74864bed0e
Tri0nic <androyd0007@yandex.ru>
2023-08-05 06:38:41 +0300

    Добавьте файлы проекта.

---
+++ Dotabuff Parser/Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotabuff_Parser
+{
+    internal class Parser
+    {
+        string url;
+
+        //"https://www.dotabuff.com/heroes/meepo/counters"
+    }
+}
---
+++ Dotabuff Parser/Program.cs
@@ -0,0 +1,10 @@
+﻿using Dotabuff_Parser;
+
+class Program
+{
+    public static void Main()
+    {
+        string heroName = Characters.dota2Heroes[15];
+        Console.WriteLine(heroName);
+    }
+}
